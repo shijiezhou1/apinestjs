864682673e2debb2eab8c3548dab05617b5a689d
Shijie Zhou <cs5129606@gmail.com>
2021-01-10 22:11:29 +0800

    Trying out the fetch

--- src/medium/medium.controller.ts
+++ src/medium/medium.controller.ts
@@ -5,8 +5,10 @@ import Parser from 'rss-parser';
 export class MediumController {
     @Get()
     async getRss() {
-        const parser = new Parser();
-        const feed = await parser.parseURL("https://medium.com/feed/@jay315");
-        return feed.items;
+
+        return await fetch('https://medium.com/feed/@jay315');
+        // const parser = new Parser();
+        // const feed = await parser.parseURL("https://medium.com/feed/@jay315");
+        // return feed.items;
     }
 }
