99d9e9be79efb3ac91fdbb63ec9191680bb562d9
Shijie Zhou <cs5129606@gmail.com>
2021-01-10 09:57:57 +0800

    Updating medium feed.

--- src/medium/medium.controller.ts
+++ src/medium/medium.controller.ts
@@ -6,7 +6,7 @@ export class MediumController {
     @Get()
     async getRss() {
         const parser = new Parser();
-        const feed = await parser.parseURL("https://medium.com/feed/@.jay");
+        const feed = await parser.parseURL("https://medium.com/feed/@jay315");
         return feed.items;
     }
 }
