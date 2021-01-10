4a426514326d6d69e6730582dd08d9323893897a
Shijie Zhou <cs5129606@gmail.com>
2021-01-10 12:21:43 +0800

    Disabled mongodb for now

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { LoginController } from './login.controller';
 import { LoginService } from './login.service';
 import { Login, LoginSchema } from '@src/schemas/login.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Login.name, schema: LoginSchema }])],
+    // imports: [MongooseModule.forFeature([{ name: Login.name, schema: LoginSchema }])],
     controllers: [LoginController],
     providers: [LoginService],
 })
