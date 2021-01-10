28b8e94be47f3fe991688ff6a66b8e9f9038e942
Shijie Zhou <cs5129606@gmail.com>
2021-01-10 22:22:31 +0800

    Removing parser for the medium package.

--- src/medium/medium.controller.ts
+++ src/medium/medium.controller.ts
@@ -1,14 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import Parser from 'rss-parser';
 
 @Controller('medium')
 export class MediumController {
     @Get()
     async getRss() {
-
         return await fetch('https://medium.com/feed/@jay315');
-        // const parser = new Parser();
-        // const feed = await parser.parseURL("https://medium.com/feed/@jay315");
-        // return feed.items;
     }
 }
