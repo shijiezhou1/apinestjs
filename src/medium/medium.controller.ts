fca33cce6ba7aba1a44016896df92e8bacea2ee7
Shijie Zhou <cs5129606@gmail.com>
2021-01-11 23:10:10 +0800

    Adding package back

--- src/medium/medium.controller.ts
+++ src/medium/medium.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { MediumService } from './medium.service';
+import Parser from 'rss-parser';
 
 @Controller('medium')
 export class MediumController {
@@ -7,6 +8,9 @@ export class MediumController {
 
     @Get()
     async getRss() {
-        return this.mediumService.findAllCats();
+        const parser = new Parser();
+        const feed = await parser.parseURL("https://medium.com/feed/@jay315");
+        return feed.items;
+        // return this.mediumService.findAllCats();
     }
 }
